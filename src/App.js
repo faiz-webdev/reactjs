bd6f1881de69bbdf0d0f2b25b4c1db6fa3637e2e
faiz-webdev <faijuddin@tothenew.com>
2023-05-26 12:21:48 +0530

    props and useState

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Greet from "./components/Greet";
+import Welcome from "./components/Welcome";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+import About from "./components/About";
+
+function App() {
+  return (
+    <>
+      {/* <Navbar title="TeXtUtils" aboutText="About website" /> */}
+      <Navbar title="TextUtils" />
+      <div className="container my-3">
+        {/* <TextForm heading='Enter the text below' /> */}
+        <About />
+      </div>
+    </>
+  );
+}
+
+export default App;
