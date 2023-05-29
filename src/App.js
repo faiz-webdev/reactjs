417054545a1e96c1bd5fd59ebbbf7c534c95c459
faiz-webdev <faijuddin@tothenew.com>
2023-05-29 13:34:59 +0530

    routing completed

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import TextForm from "./components/TextForm";
 import About from "./components/About";
 import { useState } from "react";
 import Alert from "./components/Alert";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   const [mode, setMode] = useState("light");
@@ -17,31 +18,48 @@ function App() {
       msg: mesage,
       type,
     });
-    setTimeout(() => {setAlert(null)}, 1500)
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
   };
 
   const toggleMode = () => {
     if (mode === "dark") {
       setMode("light");
       document.body.style.backgroundColor = "white";
-      showAlert('Light mode has been enabled', 'success');
-      document.title = "My react app - light mode"
+      showAlert("Light mode has been enabled", "success");
+      document.title = "My react app - light mode";
     } else if (mode === "light") {
       setMode("dark");
       document.body.style.backgroundColor = "#042743";
-      showAlert('Dark mode has been enabled', 'success');
-      document.title = "My react app - dark mode"
+      showAlert("Dark mode has been enabled", "success");
+      document.title = "My react app - dark mode";
     }
   };
   return (
     <>
       {/* <Navbar title="TeXtUtils" aboutText="About website" /> */}
-      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
-      <Alert alert={alert} />
-      <div className="container my-3">
-        <TextForm showAlert={showAlert} heading="Enter the text below" mode={mode} />
-        {/* <About /> */}
-      </div>
+      <Router>
+        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} aboutText="About" />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Routes>
+            <Route exact path="/about" element={<About />} />
+            <Route
+              path="/"
+              element={
+                <TextForm
+                  showAlert={showAlert}
+                  heading="Enter the text below"
+                  mode={mode}
+                />
+              }
+            />
+            <Route exact path="home" element={<Welcome />} />
+            {/* <About /> */}
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 export default function Navbar(props) {
   return (
@@ -7,9 +8,9 @@ export default function Navbar(props) {
       className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}
     >
       <div className="container-fluid">
-        <a className="navbar-brand" href="/">
+        <Link className="navbar-brand" to="/">
           {props.title}
-        </a>
+        </Link>
         <button
           className="navbar-toggler"
           type="button"
@@ -24,17 +25,21 @@ export default function Navbar(props) {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/">
+              <Link className="nav-link active" aria-current="page" to="/home">
                 Home
-              </a>
+              </Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/">
+              <Link className="nav-link" to="/about">
                 {props.aboutText}
-              </a>
+              </Link>
             </li>
           </ul>
-          <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'}`}>
+          <div
+            className={`form-check form-switch text-${
+              props.mode === "light" ? "dark" : "light"
+            }`}
+          >
             <input
               className="form-check-input"
               type="checkbox"
@@ -45,7 +50,7 @@ export default function Navbar(props) {
               className="form-check-label"
               htmlFor="flexSwitchCheckChecked"
             >
-              Enable {props.mode==='light'?"dark":"light"} mode
+              Enable {props.mode === "light" ? "dark" : "light"} mode
             </label>
           </div>
         </div>
