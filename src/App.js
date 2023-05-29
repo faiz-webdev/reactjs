a856945fc25fcd506b45217f8a025c32d9320861
faiz-webdev <faijuddin@tothenew.com>
2023-05-29 12:03:15 +0530

    add logo and icons

--- src/App.js
+++ src/App.js
@@ -25,10 +25,12 @@ function App() {
       setMode("light");
       document.body.style.backgroundColor = "white";
       showAlert('Light mode has been enabled', 'success');
+      document.title = "My react app - light mode"
     } else if (mode === "light") {
       setMode("dark");
       document.body.style.backgroundColor = "#042743";
       showAlert('Dark mode has been enabled', 'success');
+      document.title = "My react app - dark mode"
     }
   };
   return (
